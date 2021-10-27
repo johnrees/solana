ed86af41c3cde47196c817bea7ca080ab1f670fd
John Rees <john@bitsushi.com>
2021-10-27 19:10:03 +0100

    formatting

--- web3.js/_checksideeffects/check.js
+++ web3.js/_checksideeffects/check.js
@@ -1,47 +1,26 @@
-const glob = require("glob")
-const fs = require("fs")
-const path = require('path')
-// const { checkSideEffects } = require("check-side-effects")
-const {promisify} = require("util");
+const glob = require('glob');
+const fs = require('fs');
+const path = require('path');
+const {promisify} = require('util');
 const exec = promisify(require('child_process').exec);
 
-const outputDir = "./output"
+const outputDir = './output';
 
-glob("../dist/**/*.js", async (err,matches) => {
-
+glob('../dist/**/*.js', async (err, matches) => {
   for (const match of matches) {
-
     try {
-      const { stdout, stderr } = await exec(`check-side-effects "${match}" --warnings true`);
-
-      const cleanedOutput = stdout.replace(/(^[ \t]*\n)/gm, "").trim()
+      const {stdout, stderr} = await exec(
+        `check-side-effects "${match}" --warnings true`,
+      );
+      const cleanedOutput = stdout.replace(/(^[ \t]*\n)/gm, '').trim();
 
       if (cleanedOutput) {
-        const filePath = match.replace("../dist",outputDir)
-        fs.mkdirSync(path.dirname(filePath),{recursive: true})
-        fs.writeFileSync(filePath, cleanedOutput)
+        const filePath = match.replace('../dist', outputDir);
+        fs.mkdirSync(path.dirname(filePath), {recursive: true});
+        fs.writeFileSync(filePath, cleanedOutput);
       }
-      // console.log('stdout:', stdout);
-      // console.log('stderr:', stderr);
     } catch (e) {
-      // console.error(e); // should contain code (exit code) and signal (that caused the termination).
+      console.error(e);
     }
-
-    // checkSideEffects({
-    //   cwd: match,
-    //   // esModules,
-    //   // output,
-    //   propertyReadSideEffects: true,
-    //   globalDefs: {},
-    //   sideEffectFreeModules: [''],
-    //   resolveExternals: false,
-    //   printDependencies: false,
-    //   useBuildOptimizer: true,
-    //   useMinifier: true,
-    //   warnings: false,
-    // }).then(console.log)
-
-    // fs.writeFileSync(filePath, "")
   }
-
-})
+});
